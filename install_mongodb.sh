878138726b1be0cdc21a1e7625dac1f8291ae20c
zagretdinov-d <85208391+zagretdinov-d@users.noreply.github.com>
2021-06-24 05:03:46 +0600

    correcting

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -7,7 +7,7 @@ wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add 
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
 # обновление пакетов
-sudo apt-get update
+apt-get update
 
 # установка mongodb
 apt-get install -y mongodb-org
