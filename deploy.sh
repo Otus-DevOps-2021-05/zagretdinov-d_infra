59e0a611b4ca464d511c43e0155859ca0481daa7
zagretdinov-d <dom324255408@gmail.com>
2021-06-26 06:33:16 +0600

    deleting scripts

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# копирование кода приложения
-git clone -b monolith https://github.com/express42/reddit.git
-# установка зависимостей
-cd reddit && bundle install
-# запуск приложения
-puma -d
--- yc-vm.sh
+++
@@ -1,8 +0,0 @@
-yc compute instance create \
-	--name red-app \
-	--hostname red-app \
-	--memory=2 \
-	--create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
-	--network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
-	--metadata serial-port-enable=1 \
-  --metadata-from-file user-data=id-app.yaml
