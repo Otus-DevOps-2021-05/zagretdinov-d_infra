0e52eec658e681b3924f25fd7061782c9473bfc1
zagretdinov-d <dom324255408@gmail.com>
2021-07-04 03:01:54 +0600

    Add scripts

---
+++ terraform/files/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sleep 30
+set -e
+APP_DIR=${1:-$HOME}
+sudo kill -9 $(ps ax | grep apt | head -1 | gawk ‘{print $1}’)
+sudo apt-get install -y git
+git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+cd $APP_DIR/reddit
+bundle install
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
