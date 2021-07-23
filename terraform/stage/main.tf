57a457dd3b5baed77c9a20669cf47491490fdad7
zagretdinov-d <85208391+zagretdinov-d@users.noreply.github.com>
2021-07-24 04:26:41 +0600

    Update main.tf

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,6 +1,5 @@
 provider "yandex" {
-token = "AQAAAABV0kYTAATuwdKv8itdXUCkhp0jwTTWOjI"
-#service_account_key_file = var.service_account_key_file
+service_account_key_file = var.service_account_key_file
 cloud_id = var.cloud_id
 folder_id = var.folder_id
 zone = "ru-central1-a"
